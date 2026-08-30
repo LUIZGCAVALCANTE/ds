99e9084d7efe34b38f978f35b852b404ed1e968d
LUIZGCAVALCANTE <luizgustavolg@live.com>
2026-08-30 19:23:12 -0300

    Usuarios comuns so acessam o que pode acessar um usuario comum

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -18,13 +18,16 @@ public class DscommerceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("1 Endpoints públicos GET /produts e GET /products/{id} funcionam sem necessidade de login");
+		System.out.println("2 Endpoint de login funcionando e retornando o token de acesso");
+		System.out.println("3 Endpoints privados de produto (POST/PUT/DELETE) funcionam somente para usuário ADMIN");
+		System.out.println("4 login endpoint get/users/me retorna o usuario loggado");
+		System.out.println("5 Endpoints GET /orders/{id} e POST /orders funcionando");
+		System.out.println("6 Usuário que não é ADMIN não consegue acessar pedido que não é dele em GET /orders/{id}");
+		System.out.println("7 Rota all categories ok");
+		System.out.println(" 8 Maria só acessa o que um usuario comum pode acessar ");
 
-		System.out.println("login endpoint get/users/me retorna o usuario loggado");
-		System.out.println("Endpoint de login funcionando e retornando o token de acesso");
-		System.out.println("Endpoints privados de produto (POST/PUT/DELETE) funcionam somente para usuário ADMIN");
-		System.out.println("Endpoints GET /orders/{id} e POST /orders funcionando");
-		System.out.println("Usuário que não é ADMIN não consegue acessar pedido que não é dele em GET /orders/{id}");
-		System.out.println("Endpoints públicos GET /produts e GET /products/{id} funcionam sem necessidade de login");
+
 
 
 
